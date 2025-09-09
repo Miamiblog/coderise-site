32067fda21e85fa5fdf8098a2d6dfb92f59874cf
Miamiblog <chukwuebukaenyeribenyem@gmail.com>
2025-09-09 18:29:22 +0100

    Update and rename contact.html to contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "coderise_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "<h2>✅ Message Sent!</h2>";
+    echo "<a href='contact.html'>Go Back</a>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -102,3 +130,4 @@
 
 </body>
 </html>
+
